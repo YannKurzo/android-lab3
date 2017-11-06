d265abb03441ab80d21b4f94c21d5ed4222be3a3
yannkurzo <yann.kurzo@epfl.ch>
2017-11-06 09:04:59 +0100

    Add notification

--- Application/src/main/java/kurzo/yann/lab3/OnAlarmReceiver.java
+++ Application/src/main/java/kurzo/yann/lab3/OnAlarmReceiver.java
@@ -1,8 +1,15 @@
 package kurzo.yann.lab3;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 /**
@@ -21,5 +28,32 @@ public class OnAlarmReceiver extends BroadcastReceiver {
         BDname = intent.getStringExtra("name");
         Toast.makeText(context, "Today is " + BDname + "'s birthday !",
                 Toast.LENGTH_LONG).show();
+
+        sendNotification(context, "Birthday Notification",
+                "Today is " + BDname + "'s birthday!",
+                intent.getIntExtra("keyNotification", 0));
+    }
+
+    private void sendNotification(Context context, String title, String text, int id) {
+        NotificationManagerCompat mNotificationManager = NotificationManagerCompat.from(context);
+        NotificationCompat.Builder mNotification = new NotificationCompat.Builder(context);
+
+        mNotification.setContentTitle(title);
+        mNotification.setContentText(text);
+        mNotification.setSmallIcon(R.drawable.birthday_cake);
+        mNotification.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.birthday_cake));
+
+        mNotification.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+        Uri uriSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        mNotification.setSound(uriSound);
+
+        Intent intent = new Intent(context,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, id, intent,0);
+        mNotification.setContentIntent(pendingIntent);
+        mNotification.setAutoCancel(true);
+        mNotificationManager.notify(id, mNotification.build());
+
+        int idNotification = intent.getIntExtra("keyNotification",0);
+        sendNotification(context,"Birthday Notification","Today is " + BDname + "'s birthday !", idNotification);
     }
 }
